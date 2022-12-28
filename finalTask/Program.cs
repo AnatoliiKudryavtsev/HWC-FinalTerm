0d021cea250304800b7b09b0b3a36d8dc7eda921
Анатолий <Anatolii>
2022-12-28 18:18:21 +0300

    add finalTask/

---
+++ finalTask/Program.cs
@@ -0,0 +1,39 @@
+﻿//string[] array1 = {"hello", "2", "world", ":-)"};
+//string[] array2 = {"1234", "1567", "-2", "computer science", "**"};
+string[] array3 = {"Russia", "Denmark", "Kazan"};
+
+
+void PrintStringArray(string[] array)
+{
+    int length = array.Length;
+    string result = String.Empty;
+    for (int i = 0; i < length; i++)
+        {
+            result = result + array[i] + ", ";
+        }
+    if (result.Length > 1) result = result.Substring(0, result.Length - 2);
+    Console.Write("[" + result + "]");
+}
+
+string[] ChangeArray(string[] array)
+{
+    int count = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(array[i].Length < 4) count++;
+    }
+
+    string[] newArray = new string[count];
+    int j = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if(array[i].Length < 4) {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+PrintStringArray(ChangeArray(array3));
